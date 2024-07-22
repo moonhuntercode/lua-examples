1dfe119f1b697eeff2ba96f1869fb9e8d0497f56
victor code <53928868+moonhuntercode@users.noreply.github.com>
2024-07-21 23:26:09 -0400

    Create corutina.lua
    examples

---
+++ corutina.lua
@@ -0,0 +1,15 @@
+-- Definir una función como corutina
+function ejemplo_corutina()
+    for i = 1, 5 do
+        print("Corutina paso " .. i)
+        coroutine.yield()  -- Suspender la ejecución
+    end
+end
+
+-- Crear una corutina
+co = coroutine.create(ejemplo_corutina)
+
+-- Reanudar la corutina
+for i = 1, 5 do
+    coroutine.resume(co)
+end
